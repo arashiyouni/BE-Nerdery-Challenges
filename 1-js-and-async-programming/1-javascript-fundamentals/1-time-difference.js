05aff06f521afb041b2d5f012e0096c292853888
arashiyouni <angie.espinoza.campos@gmail.com>
2025-07-16 15:22:53 -0600

    feat (js-fundamentals): add resolution time difference

--- 1-js-and-async-programming/1-javascript-fundamentals/1-time-difference.js
+++ 1-js-and-async-programming/1-javascript-fundamentals/1-time-difference.js
@@ -16,6 +16,22 @@ timeDifference(7200, 3400); // Expected output: "01:03:20"
 
 */
 
-const timeDifference = (a, b) => {};
+const timeDifference = (a, b) => {
+  const secondMax = Math.max(a, b);
+  const secondMin = Math.min(a, b);
+
+  const diffInSecond = secondMin - secondMax;
+
+  const timeSecond = 3600;
+  const timeMinutes = 60;
+
+  const hoursTime = Math.ceil(diffInSecond / timeSecond);
+  const minutesTime = Math.ceil(diffInSecond / timeMinutes) % 60;
+  const secondTime = Math.ceil(diffInSecond % 60);
+
+  console.log(hoursTime, minutesTime, secondTime);
+
+  return `${Math.abs(hoursTime) <= 9 ? `0${Math.abs(hoursTime)}` : Math.abs(hoursTime)}:${Math.abs(minutesTime) <= 9 ? `0${Math.abs(minutesTime)}` : Math.abs(minutesTime)}:${Math.abs(secondTime) <= 9 ? `0${Math.abs(secondTime)}` : Math.abs(secondTime)}`;
+};
 
 module.exports = timeDifference;

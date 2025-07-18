56e9b55d26a1624474173481f0e89106e421e6e8
arashiyouni <angie.espinoza.campos@gmail.com>
2025-07-18 12:52:21 -0600

    feat: add resolved promise

--- 1-js-and-async-programming/2-asynchronous-js/2-promise.js
+++ 1-js-and-async-programming/2-asynchronous-js/2-promise.js
@@ -4,13 +4,19 @@
   Get a list of users who have rated more movies negatively than positively.
 
   Use the methods in utils/mocked-api to retrieve user and rating data.
-  Check how many movies each user liked and disliked, then return only those with more dislikes.
+  !Check how many movies each user liked and disliked, then return only those with more dislikes.
 
   Requirements:
   - Use only Promise static methods (e.g., Promise.all, Promise.then, etc.) to handle the results
   - Only print the user information in the output—no extra text or formatting
 
  */
+
+const {
+  getUsers,
+  getLikedMovies,
+  getDislikedMovies,
+} = require("./utils/mocked-api");
 
 /**
  * @typedef {Object} User
@@ -26,13 +32,35 @@
  */
 const getUsersWithMoreDislikedMoviesThanLikedMovies = () => {
   // Add your code here
+  try {
+    return Promise.all([
+      getUsers(),
+      getLikedMovies(),
+      getDislikedMovies(),
+    ]).then(([users, moviesLikes, moviesDislikes]) => {
+      const response = users.filter((user) => {
+        const userLikes = moviesLikes.find((like) => like.userId === user.id);
+        const userDislikes = moviesDislikes.find(
+          (dislike) => dislike.userId === user.id,
+        );
 
+        const likeCount = userLikes ? userLikes.movies.length : 0;
+        const dislikeCount = userDislikes ? userDislikes.movies.length : 0;
+
+        return dislikeCount > likeCount;
+      });
+
+      return response;
+    });
+  } catch (err) {
+    console.log("Error to any Promise", err);
+  }
   return [];
 };
 
 getUsersWithMoreDislikedMoviesThanLikedMovies().then((users) => {
   console.log("Users with more disliked movies than liked movies:");
   users.forEach((user) => {
-    console.log(user, age);
+    console.log(user);
   });
 });
